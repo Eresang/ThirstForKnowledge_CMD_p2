c47dbcb4d023302ca451f3b54547e49c209a22b0
Elroy Andela <elroyandela@gmail.com>
2014-12-16 13:56:17 +0100

    Main game state added

--- loadlibs.js
+++ loadlibs.js
@@ -3,6 +3,7 @@ console.log("%c ----------------------------------- ", "color:black; background:
 // add states to the Phaser.Game object
 game.state.add("Boot", boot);
 game.state.add("Loading", loading);
+game.state.add("Runner", runner);
 // --
 console.log("%c ----------------------------------- ", "color:black; background:yellow");
 // start at the beginning
