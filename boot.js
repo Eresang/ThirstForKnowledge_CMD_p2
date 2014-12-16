159a7696166c63509a29052328bfbdb71fab7080
khollord <rimmervanderwal@gmail.com>
2014-12-16 10:28:57 +0100

    Initial boot file
    Contains Phaser.game object declaration and internal gameworld
    dimensions for global use. Sets up a loading bar image and sets up the
    built in Phaser.ScaleManager, then passes to the loading gamestate.

---
+++ boot.js
@@ -0,0 +1,35 @@
+// initial internal game world dimensions
+var gameWidth = 640,
+    gameHeight = 480;
+// create the game object and set it to create a canvas in parent container 'gameDiv'
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'gameDiv');
+
+// the 'boot' game state;
+// it loads the loading bar graphic and sets up a couple of scaling options,
+// and then it starts the 'loading' game state
+var boot = function (game) {
+    'use strict';
+    console.log("%c        boot.js is being processed   ", "color:white; background:red");
+};
+
+boot.prototype = {
+    preload: function () {
+        'use strict';
+        this.load.image("loadBar", "assets/loading.png");
+        game.stage.backgroundColor = '#00001a';
+    },
+    create: function () {
+        'use strict';
+        // set up scale manager
+	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = gameWidth / 2;
+        this.scale.minHeight = gameHeight / 2;
+        this.scale.maxWidth = gameWidth;
+        this.scale.maxHeight = gameHeight;
+	this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        // refresh layout
+	this.scale.refresh();
+	this.state.start("Loading");
+    }
+};
