b402e2fb9c5e5ad0fd21f2777e3c1cb6e07b375a
khollord <rimmervanderwal@gmail.com>
2014-12-16 10:57:59 +0100

    New files and small edit
    Removed code related to old prototype.

---
+++ loading.js
@@ -0,0 +1,26 @@
+// the 'loading' game state;
+// it sets a loading bar graphic loaded in 'preload' game state,
+// and then continues to load all the required assets
+var loading = function (game) {
+    'use strict';
+	console.log("%c     loading.js is being processed   ", "color:white; background:red");
+};
+
+loading.prototype = {
+    preload: function () {
+    	'use strict';
+        // set up loading bar graphic
+        var loadingBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, "loadBar");
+        loadingBar.anchor.setTo(0.5, 0.5);
+        this.load.setPreloadSprite(loadingBar, 1);
+        
+        // insert the loading of all assets after this line, :
+        // this.load.image('assetname', 'assets/ts_t0.png', 48, 48);
+
+    },
+    create: function () {
+        'use strict';
+        // everything is loaded, time to start the game
+	// this.game.state.start("nextstate", true, false, true);
+    }
+};
--- loadlibs.js
+++ loadlibs.js
@@ -3,8 +3,6 @@ console.log("%c ----------------------------------- ", "color:black; background:
 // add states to the Phaser.Game object
 game.state.add("Boot", boot);
 game.state.add("Loading", loading);
-game.state.add("GameStart", gamestart);
-game.state.add("GameOver", gameover);
 // --
 console.log("%c ----------------------------------- ", "color:black; background:yellow");
 // start at the beginning
